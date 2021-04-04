408842aad17907883c1d1ad60a5f653092197b25
Nawod <nawodrandima64@gmail.com>
2021-04-04 14:58:40 +0530

    first commit

---
+++ src/DB/DBConnection.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Nawod
+ */
+public class DBConnection {
+    Connection con = null;
+    
+    public static Connection getConnection(){
+        
+        try {
+            Class.forName("org.sqlite.JDBC");                 
+            Connection con = DriverManager.getConnection("jdbc:sqlite:TimeTableSystem.db");
+            
+            System.out.println("Connection Succesfull");
+            
+            return con;
+            
+        } catch (Exception e) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+            return null;   
+        }
+    }
+    
+    public static void main(String[] args){
+        getConnection();
+    }
+}
