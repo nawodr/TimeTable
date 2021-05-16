ffe62d348ecfbc4e0247df75e4f398c834b9195f
Nawod <nawodrandima64@gmail.com>
2021-05-17 00:54:25 +0530

    database Create Part

--- src/DB/DBConnection.java
+++ src/DB/DBConnection.java
@@ -5,40 +5,59 @@
  */
 package DB;
 
+import CreateTable.CreateTable;
+import com.mysql.jdbc.exceptions.MySQLSyntaxErrorException;
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import static jdk.javadoc.internal.doclets.toolkit.util.VisibleMemberTable.Kind.PROPERTIES;
+import jdk.vm.ci.meta.Constant;
 
 /**
  *
  * @author Nawod
  */
 public class DBConnection {
-    Connection con = null;
-    
-    public static Connection getConnection(){
-        
-        try {
-            Class.forName("org.sqlite.JDBC");                 
-            Connection con = DriverManager.getConnection("jdbc:sqlite:db.db");
-            
-//            System.out.println("Connection Succesfull");
 
-            if(con == null || con.isClosed()){
-                Class.forName("org.sqlite.JDBC");                 
-                con = DriverManager.getConnection("jdbc:sqlite:db.db");
-                System.out.println("Connection Succesfull");
-            }
-            
-            return con;
-            
-        } catch (Exception e) {
-            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-            System.exit(0);
-            return null;   
-        }
+    private static Connection con = null;
+    private static String DBpath = "db.db";
+
+    public static boolean isDatabaseExist(String dbPath) {
+        File file = new File(dbPath);
+        return file.exists();
     }
-    
-    public static void main(String[] args){
-        getConnection();
+
+    public static Connection getConnection() {
+        if (isDatabaseExist(DBpath)) {
+            //DBpath = path.concat("\\db.db");
+
+            try {
+
+                Class.forName("org.sqlite.JDBC");
+                Connection con = DriverManager.getConnection("jdbc:sqlite:db.db");
+
+//            System.out.println("Connection Succesfull");
+                if (con == null || con.isClosed()) {
+                    Class.forName("org.sqlite.JDBC");
+                    con = DriverManager.getConnection("jdbc:sqlite:db.db");
+                    System.out.println("Connection Succesfull");
+                }
+
+                return con;
+
+            } catch (Exception e) {
+                System.err.println(e.getClass().getName() + ": " + e.getMessage());
+                System.exit(0);
+                return null;
+            }
+        } else {
+//            System.out.println("else => ");
+            CreateTable.createDatabase(DBpath);
+
+        }
+        return con;
+
     }
 }
